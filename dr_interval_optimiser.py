ace20d45517ac9ab5be564c802c4fa4274e357e3
Ruba18-Code <rgithub.cornhusk791@passmail.net>
2025-06-13 14:33:36 +0200

    Created a program to find the best delta R interval for the ROC curve using the ROC_curve_optimiser function. See: dr_interval_optimiser.py

---
+++ dr_interval_optimiser.py
@@ -0,0 +1,36 @@
+# %%
+from my_functions import*
+
+# %%
+file= uproot.open("/home/ruben/Escritorio/BachelorThesisRuben/Data/Muon_trees.root") #opening the Root file with Uproot 
+MuonTree_ZeroBias=file["MuonTree_ZeroBias;1"]
+MuonTree_Zmumu=file["MuonTree_Zmumu;1"]
+
+nmin=10000
+nmax=15000
+
+#Select quality 0 Z->mumu
+Zmumu_pt=quality_selector(MuonTree_Zmumu["muon_quality"].array(),MuonTree_Zmumu["muon_pt"].array(),0)[nmin:nmax]
+Zmumu_eta=quality_selector(MuonTree_Zmumu["muon_quality"].array(),MuonTree_Zmumu["muon_eta"].array(),0)[nmin:nmax]
+Zmumu_phi=quality_selector(MuonTree_Zmumu["muon_quality"].array(),MuonTree_Zmumu["muon_phi"].array(),0)[nmin:nmax]
+
+#And select the Z peak pairs
+Zmumu_pt, Zmumu_eta, Zmumu_phi = get_all_Z_peak_pairs(Zmumu_pt,Zmumu_eta,Zmumu_phi)
+
+
+#Select the ZeroBias data with energy cut
+ZeroBias_eta=energy_cut(MuonTree_ZeroBias["muon_pt"].array(), MuonTree_ZeroBias["muon_eta"].array())[nmin:nmax]
+ZeroBias_phi=energy_cut(MuonTree_ZeroBias["muon_pt"].array(), MuonTree_ZeroBias["muon_phi"].array())[nmin:nmax]
+ZeroBias_pt=energy_cut(MuonTree_ZeroBias["muon_pt"].array(), MuonTree_ZeroBias["muon_pt"].array())[nmin:nmax]
+
+Zmumu_range=[nmin,nmax]
+ZeroBias_range=[nmin,nmax]
+
+bins=np.linspace(0,3,1000)
+iterations=200
+
+dr, FPR= optimise_ROC_curve(MuonTree_Zmumu, MuonTree_ZeroBias, Zmumu_pt, Zmumu_eta, Zmumu_phi, ZeroBias_pt, ZeroBias_eta, ZeroBias_phi,
+                    Zmumu_range,ZeroBias_range, bins, iterations)
+
+print(dr, FPR)
+# %%
--- jTower_cut_Z_pairs_plots.py
+++ jTower_cut_Z_pairs_plots.py
@@ -11,9 +11,9 @@ MuonTree_Zmumu=file["MuonTree_Zmumu;1"]
 
 #Choose the range of events to plot
 nmin=0
-nmax=1000
-dr_min=0.3101
-dr_max=0.5820
+nmax=2000
+dr_min=0.2
+dr_max=0.6
 
 #Select quality 0 Z->mumu
 Zmumu_pt=quality_selector(MuonTree_Zmumu["muon_quality"].array(),MuonTree_Zmumu["muon_pt"].array(),0)[nmin:nmax]
@@ -48,7 +48,7 @@ labels=[fr"Zero Bias (cut=14GeV), events={non_empty_count1}",fr"Z $\longrightarr
 
 #Plot the data
 coolplot([data1,data2],np.linspace(0,20000,40),colors,labels, "Isolation (MeV)","Counts",
-         r"Isolation histogram, $\Delta R= [0.3101,0.5820]$")
+         r"Isolation histogram, $\Delta R= [0.2,0.6]$")
 
 # %%
 #Prepare the data
@@ -60,7 +60,7 @@ colors=["#0072B2", "#FD0000"]
 labels=[fr"Zero Bias (cut=14GeV), events={non_empty_count1}",fr"Z $\longrightarrow \mu \mu$ (Q=0, Z peak pairs), events={non_empty_count2}"]
 
 coolplot([ratio1,ratio2],np.linspace(0,0.6,40),colors,labels, "Isolation/et ratio","Counts",
-         r"$\frac{E_{t,iso}}{E_{t}}$ ratio, $\Delta R= [0.3101,0.5820]$")
+         r"$\frac{E_{t,iso}}{E_{t}}$ ratio, $\Delta R= [0.2,0.6]$")
 
 # %%
 #Now let's do the ROC curve
@@ -69,8 +69,8 @@ coolplot([ratio1,ratio2],np.linspace(0,0.6,40),colors,labels, "Isolation/et rati
 #Number of bins is the square root of the number of events 
 #The range starts at -1/sqrt(nmax-nmin), this ensures that the data starts at (0,0) even on extreme cases
 bins=np.linspace(0,1,5*int(np.sqrt(nmax-nmin)))
-dr_min=[0.10,0.2,0.3101,0.0]
-dr_max=[0.45,0.8,0.5820,0.4]
+dr_min=[0.2,0.3,0.1]
+dr_max=[0.8,0.6,0.4]
 
 #Prepare the data for the plots
 plot_ROC_curve(MuonTree_Zmumu, MuonTree_ZeroBias, Zmumu_pt, Zmumu_eta, Zmumu_phi, ZeroBias_pt, ZeroBias_eta,
